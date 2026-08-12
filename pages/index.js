5236b7e417a00358d8b9c12ce7d210138b1df021
Ogaalsan <Ogaalsancon@gmail.com>
2026-08-12 11:34:30 +0300

    Refactor dynamic imports to static imports

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,17 @@
-import dynamic from "next/dynamic";
 import Layout from "@/components/layout/Layout";
 import Banner from "@/components/sections/home/Banner";
 import Features from "@/components/sections/home/Features";
 import About from "@/components/sections/home/About";
 import Services from "@/components/sections/home/Services";
+import Overview from "@/components/sections/home/Overview";
+import Choose from "@/components/sections/home/Choose";
+import Project from "@/components/sections/home/Project";
+import Cta from "@/components/sections/home/Cta";
+import FocusAreas from "@/components/sections/home/FocusAreas";
+import Blog from "@/components/sections/home/Blog";
 import { fetchActiveServices } from "@/lib/services";
 import { fetchPublishedPosts } from "@/lib/posts";
 import { useClientFetch } from "@/hooks/useClientFetch";
-
-const Overview = dynamic(() => import("@/components/sections/home/Overview"), {
-  loading: () => <div style={{ minHeight: "200px" }} />,
-});
-const Choose = dynamic(() => import("@/components/sections/home/Choose"), {
-  loading: () => <div style={{ minHeight: "200px" }} />,
-});
-const Project = dynamic(() => import("@/components/sections/home/Project"), {
-  loading: () => <div style={{ minHeight: "200px" }} />,
-});
-const Cta = dynamic(() => import("@/components/sections/home/Cta"), {
-  loading: () => <div style={{ minHeight: "200px" }} />,
-});
-const FocusAreas = dynamic(() => import("@/components/sections/home/FocusAreas"), {
-  loading: () => <div style={{ minHeight: "200px" }} />,
-});
-const Blog = dynamic(() => import("@/components/sections/home/Blog"), {
-  loading: () => <div style={{ minHeight: "200px" }} />,
-});
 
 export default function Home() {
   const { data: services = [], loading: servicesLoading } = useClientFetch(
